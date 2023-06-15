92d0da6e55e9e085578f3999836d6942c29222a5
The_O_G_Sotun <oluwasotunogunjirin@gmail.com>
2023-06-15 11:38:06 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+// variables
+let reactBtn = document.querySelector('#react-project');
+let bootstrapBtn = document.querySelector('#bootstrap-project');
+let responsiveBtn = document.querySelector('#responsive-project');
+let tailwindBtn = document.querySelector('#tailwindproject');
+let lesserThan = document.querySelector('#less');
+let one = document.querySelector('.one');
+let two = document.querySelector('.two');
+let three = document.querySelector('.three');
+let ten = document.querySelector('.ten');
+let greaterThan = document.querySelector('#greater');
+
+
+
+
+// code
+reactBtn.addEventListener('click', function(){
+    alert('This is not available yet')
+})
+
+bootstrapBtn.addEventListener('click', function(){
+    alert('This is not available yet')
+});
+
+responsiveBtn.addEventListener('click', function(){
+    alert('This is the active project')
+});
+
+tailwindBtn.addEventListener('click', function(){
+    alert('This is not available yet')
+});
+
+lesserThan.addEventListener('click', function(){
+    alert('This does not work yet')
+});
+
+one.addEventListener('click', function(){
+    alert('This does not work yet')
+});
+
+two.addEventListener('click', function(){
+    alert('This is the active page')
+});
+
+three.addEventListener('click', function(){
+    alert('This does not work yet')
+});
+
+ten.addEventListener('click', function(){
+    alert('This does not work yet')
+});
+
+greaterThan.addEventListener('click', function(){
+    alert('This does not work yet')
+});
+
+
